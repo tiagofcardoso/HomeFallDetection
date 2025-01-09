d7f8c63125601653a1c40278719d23b9b1314429
Tiago Cardoso <tiagofabriciocardoso@gmail.com>
2025-01-09 19:49:03 +0000

    Fall detection version1

--- fall_detection/config.py
+++ fall_detection/config.py
@@ -1,9 +1,9 @@
 import cv2
 
-TWILIO_ACCOUNT_SID = 'AC7d746a6c25ce284435434b65f7de6ea8'
-TWILIO_AUTH_TOKEN = '559080bd8c51be7fffc0f92bb74cf6e5'
+TWILIO_ACCOUNT_SID = 'YOUR_TWILIO_ACCOUNT_SID'
+TWILIO_AUTH_TOKEN = 'YOUR_TWILIO_AUTH_TOKEN'
 TWILIO_FROM = 'whatsapp:+14155238886'
-TWILIO_TO = 'whatsapp:+351969683415'
+TWILIO_TO = 'whatsapp:+14155238886'
 
 VIDEO_SOURCE = 2  # default to external webcam
 try:
@@ -13,7 +13,7 @@ try:
         cap = cv2.VideoCapture(VIDEO_SOURCE)
     cap.release()
 except ImportError:
-    VIDEO_SOURCE = 0  
+    VIDEO_SOURCE = 0
 
 VIDEO_DIR = 'fall_detection/videos'
 VIDEO_OUTPUT = 'falldown.mp4'
